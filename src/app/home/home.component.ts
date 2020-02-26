af8e4d37cbf15c18c050b0724e7d63cd8c0ca93b
Mohan <mohan.naidu@gmail.com>
2020-02-26 18:06:55 +0800

    added material and mat-card

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
+})
+@NgModule({
+  imports: [
+  ],
 })
 export class HomeComponent implements OnInit {
   title = 'Malaysia Story';
